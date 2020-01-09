62ad6fe95c3183fe4bd8bbd05435869d5028fd06
Audrey Ly <aly1@a-139160.ups.u-psud.fr>
2020-01-09 17:10:50 +0100

    ajout du test de puissance

--- test_unitaire.py
+++ test_unitaire.py
@@ -4,18 +4,22 @@ import unittest
 class Test_calculatrice(unittest.TestCase):
 
     def test_addition(self):
+
         calculatrice = Calculatrice()
             
         res_addition = calculatrice.addition(2,3)
 
         self.assertEqual(res_addition,5)
 
+
     def test_soustraction(self):
+
         calculatrice = Calculatrice()
         
         res_soustraction = calculatrice.soustraction(5,3)
         
         self.assertEqual(res_soustraction,2)
+
 
     def test_multiplication(self):
         
@@ -29,7 +33,9 @@ class Test_calculatrice(unittest.TestCase):
         
         self.assertEqual(res_multiplication,res_multiplication2)
 
+
     def test_division(self):
+
         calculatrice = Calculatrice()
         
         res_division = calculatrice.division(5,2)
@@ -37,6 +43,14 @@ class Test_calculatrice(unittest.TestCase):
         self.assertEqual(res_division,2.5)
         
         self.assertRaises(Exception, calculatrice.division,5,0)
+        
+    def test_puissance(self):
+        
+        calculatrice = Calculatrice()
+        
+        res_puissance = calculatrice.puissance(2,3)
+        
+        self.assertEqual(res_puissance, 8)
 
 
 if __name__ == '__main__':
