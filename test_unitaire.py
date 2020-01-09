e2021da29315bdcdb0b3bbe43cd4c27d84376563
Audrey Ly <aly1@a-139160.ups.u-psud.fr>
2020-01-09 16:21:53 +0100

    Ajout de tests unitaires

--- test_unitaire.py
+++ test_unitaire.py
@@ -3,23 +3,40 @@ import unittest
 
 class Test_calculatrice(unittest.TestCase):
 
-	def test_addition(self):
-		lexical = Lexical("main.c")
-		calculatrice = Calculatrice()
+    def test_addition(self):
+        calculatrice = Calculatrice()
+            
+        res_addition = calculatrice.addition(2,3)
 
-		res_addition = calculatrice.addtion(2,3)
+        self.assertEqual(res_addition,5)
 
-		self.assertEqual(res_addition,5)
+    def test_soustraction(self):
+        calculatrice = Calculatrice()
+        
+        res_soustraction = calculatrice.soustraction(5,3)
+        
+        self.assertEqual(res_soustraction,2)
 
-	def test_soustraction(self):
-		pass
+    def test_multiplication(self):
+        
+        calculatrice = Calculatrice()
+        
+        res_multiplication = calculatrice.multiplication(2,5)
+        
+        self.assertEqual(res_multiplication,10)
+        
+        res_multiplication2 = calculatrice.multiplication(5,2)
+        
+        self.assertEqual(res_multiplication,res_multiplication2)
 
-	def test_multiplication(self):
-		pass
-
-	def test_division(self):
-		pass
-
+    def test_division(self):
+        calculatrice = Calculatrice()
+        
+        res_division = calculatrice.division(5,2)
+        
+        self.assertEqual(res_division,2.5)
+        
+        self.assertRaises(Exception, calculatrice.division,5,0)
 
 
 if __name__ == '__main__':
